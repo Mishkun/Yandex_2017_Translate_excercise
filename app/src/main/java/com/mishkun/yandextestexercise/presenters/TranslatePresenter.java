4655df9ecb32de566f3819bc44c468d05730a081
mishkun.lev <mishkun.lev@gmail.com>
2017-03-29 00:13:33 +0300

    Added shell TranslatePresenter

---
+++ app/src/main/java/com/mishkun/yandextestexercise/presenters/TranslatePresenter.java
@@ -0,0 +1,12 @@
+package com.mishkun.yandextestexercise.presenters;
+
+/**
+ * Created by Mishkun on 28.03.2017.
+ */
+
+public interface TranslatePresenter {
+    void OnReverseTranslationButtonClicked();
+    void OnToDirectionButtonClicked();
+    void OnFromDirectionButtonClicked();
+    void OnFavoriteButtonClicked();
+}
---
+++ app/src/main/java/com/mishkun/yandextestexercise/presenters/TranslatePresenterImpl.java
@@ -0,0 +1,30 @@
+package com.mishkun.yandextestexercise.presenters;
+
+import com.arellomobile.mvp.MvpPresenter;
+import com.mishkun.yandextestexercise.views.TranslateView;
+
+/**
+ * Created by Mishkun on 28.03.2017.
+ */
+
+class TranslatePresenterImpl extends MvpPresenter<TranslateView> implements TranslatePresenter {
+
+    public void OnReverseTranslationButtonClicked() {
+
+    }
+
+
+    public void OnToDirectionButtonClicked() {
+
+    }
+
+
+    public void OnFromDirectionButtonClicked() {
+
+    }
+
+
+    public void OnFavoriteButtonClicked() {
+
+    }
+}
